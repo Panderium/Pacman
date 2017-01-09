3fbb51eaed3bca6beba38c8b712a4e92a400b9fc
Jérémy <Jrmy@MON-PC>
2017-01-09 11:42:55 +0100

    commit initial

---
+++ Pacman/Pacman/Pacman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+public class Pacman : ObjetAnime
+{
+	public Pacman()
+	{
+        super(Content.Load<Texture2D>("Pacman"), new Vector2(0f, 0f), new Vector2(20f, 20f));
+	}
+}
